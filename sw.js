f77c77fa73d4b36881f7a2defbb62db497e3c8fd
0Fluff <jayancobky567@gmail.com>
2026-01-04 02:19:54 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "ofluff-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./icon.svg",
+  "./manifest.json"
+];
+
+// Install Event: Cache core files
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Fetch Event: Serve from cache, fall back to network
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
